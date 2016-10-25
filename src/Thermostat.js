70d50a69ffc2b51104c7364775be27056896fa96
Amaal Ali <amq.ali@gmail.com>
2016-10-25 15:24:25 +0100

    Refactored Thermostat

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,9 +1,10 @@
 "use strict";
 
 function Thermostat(){
-  this._degrees = 20;
+  this._DEFAULT_TEMPERATURE = 20;
+  this._temperature = this._DEFAULT_TEMPERATURE;
 };
 
 Thermostat.prototype.temperature = function () {
-  return this._degrees;
+  return this._temperature;
 };
