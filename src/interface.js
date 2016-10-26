f3b8441e792b7ca01ad2b022d98fb29cd552ecaa
crispinandrews <crispin.andrews@gmail.com>
2016-10-26 15:27:10 +0100

    HTML form done. jQuery started

---
+++ src/interface.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+  updateTemperature();
+
+  $('#temperature-up').on('click', function() { // event listener
+    thermostat.increaseTemperature(); // update model
+    updateTemperature();
+  })
+
+  $('#temperature-down').click(function() { // event listener
+    thermostat.decreaseTemperature(); // update model
+    updateTemperature();
+  })
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+  }
+})
