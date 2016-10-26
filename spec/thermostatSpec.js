8ec4cb1ff894a26ab0b0337494fe5465855e7491
Stephen Gregory <stejgregory@gmail.com>
2016-10-26 10:56:45 +0100

    Created Thermostat spec and Source file. Spec for Increase temp.

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('increases in temperature with the up button', function() {
+    thermostat.increaseTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+});
