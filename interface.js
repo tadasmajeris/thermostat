b320363f5947c70c31264882ea481f36a5011402
michkles <michkles@gmail.com>
2016-10-26 15:29:52 +0100

    step6 all buttons work

--- interface.js
+++ interface.js
@@ -1,23 +1,41 @@
 $(document).ready(function() {
 
   var thermostat = new Thermostat();
+  updateTemperature();
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+  }
 
   $('#temperature').text(thermostat.temperature);
 
   $('#temperature-up').on('click', function() {
     thermostat.increaseTemperature(1);
-    $('#temperature').text(thermostat.temperature);
+    updateTemperature();
   });
 
 
   $('#temperature-down').on('click', function() {
     thermostat.decreaseTemperature(1);
-    $('#temperature').text(thermostat.temperature);
+    updateTemperature();
   });
 
+  $('#temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
 
+  $('#powersaving-on').click(function() {
+    thermostat.switchOn()
+    $('#power-saving-status').text('on')
+    updateTemperature();
+  });
 
-
+  $('#powersaving-off').click(function() {
+    thermostat.switchOff();
+    $('#power-saving-status').text('off')
+    updateTemperature();
+  });
 
 
 
