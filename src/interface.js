40fe6075be8b919f3ff727ede9eff9eb192eec25
Stephen Gregory <stejgregory@gmail.com>
2016-10-26 15:55:58 +0100

    Added jQuery and CSS stylesheet

--- src/interface.js
+++ src/interface.js
@@ -12,7 +12,25 @@ $(document).ready(function() {
     updateTemperature();
   })
 
+  $('#temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  $('#powersaving-on').click(function() {
+    thermostat.turnPowerSavingModeOn();
+    $('#power-saving-status').text('on')
+    updateTemperature();
+  })
+
+  $('#powersaving-off').click(function() {
+    thermostat.turnPowerSavingModeOff();
+    $('#power-saving-status').text('off')
+    updateTemperature();
+  })
+
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
   }
 })
