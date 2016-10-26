76ce11b09873d5a41b19bf9fd2b73a7e95277df4
crispinandrews <crispin.andrews@gmail.com>
2016-10-26 11:33:45 +0100

    Increase/decrease temp specs done. Power saving mode on be default

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -16,4 +16,25 @@ describe('Thermostat', function() {
     thermostat.increaseTemperature();
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
+
+  it('decreases in temperature with the down button', function(){
+    thermostat.decreaseTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a minimum temperature of 10 degrees', function(){
+    for (var i = 0; i < 11; i++){
+      thermostat.decreaseTemperature();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it('has power saving mode on by default', function(){
+    expect(thermostat.isPowerSavingModeOn()).toBe(true)
+  });
+
+  it('can turn power saving mode off', function(){
+    thermostat.turnPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false)
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,8 +2,29 @@
 
 function Thermostat() {
   this.temperature = 20;
+  this.MINIMUM_TEMPERATURE = 10;
+  this.powerSavingModeOn = true;
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
+};
+
+Thermostat.prototype.increaseTemperature = function(){
+  this.temperature += 1;
+};
+
+Thermostat.prototype.decreaseTemperature = function(){
+  if (this.isMinimumTemperature()) {
+    return;
+  }
+  this.temperature -= 1;
+};
+
+Thermostat.prototype.isMinimumTemperature = function(){
+  return this.temperature === this.MINIMUM_TEMPERATURE;
+};
+
+Thermostat.prototype.isPowerSavingModeOn = function(){
+  return this.powerSavingModeOn === true;
 };
